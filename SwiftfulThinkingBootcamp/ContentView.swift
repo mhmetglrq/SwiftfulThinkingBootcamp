0e056a60a09ddb7d54ea3eda23d09c7601281f4b
Mehmet Güler <mhmetglr.q@gmail.com>
2026-02-02 22:19:29 +0300

    Initial Commit

---
+++ SwiftfulThinkingBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mehmet Güler on 2.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mehmet Güler on 2.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
