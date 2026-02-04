e4503739ab4db98267a88dcf0cdfe264dba7cd36
Mehmet Güler <mhmetglr.q@gmail.com>
2026-02-04 19:04:31 +0300

    video 6

---
+++ SwiftfulThinkingBootcamp/Icons.swift
@@ -0,0 +1,31 @@
+//
+//  Icons.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mehmet Güler on 4.02.2026.
+//
+
+import SwiftUI
+
+struct Icons: View {
+    var body: some View {
+        Image(systemName: "pencil.tip.crop.circle.fill",)
+        
+            .symbolRenderingMode(.monochrome)
+            .font(.largeTitle)
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .scaledToFill()
+//            .font(.largeTitle)
+//            .font(.system(size: 200))
+//            .foregroundStyle(Color.green)
+//            .frame(width: 300,height: 300)
+//            .clipped()
+        
+            
+    }
+}
+
+#Preview {
+    Icons()
+}
